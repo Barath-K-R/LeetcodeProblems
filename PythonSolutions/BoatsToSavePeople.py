d52152ed48a1b64552b5b069f7f9c0ac31db4b76
Barath-K-R <barath223637@gmail.com>
2024-09-26 22:45:53 +0530

    Boats to save people

---
+++ PythonSolutions/BoatsToSavePeople.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def numRescueBoats(self, people, limit):
+        people.sort()
+        right = len(people) - 1
+        left = res = 0
+        while left <= right:
+            if people[left] + people[right] <= limit:
+                left += 1
+            right -= 1
+            res += 1
+        return res
+        
